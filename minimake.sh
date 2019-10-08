4bba1cdc7e6ea15bb73d0eb31ff3275f00cb0c5d
Pim Snel <pim@lingewoud.nl>
2019-10-08 16:58:31 +0200

    update code comments

--- minimake.sh
+++ minimake.sh
@@ -6,7 +6,7 @@
 #   | |  | || || |\  || |  | |  | |/ ___ \| . \| |___
 #   |_|  |_|___|_| \_|___| |_|  |_/_/   \_\_|\_\_____|
 
-## 1: MINIMAKE INIT. PUT THIS IN THE TOP OF YOUR SCRIPT
+## 1: MINIMAKE INIT. PLACE THIS IN THE TOP OF YOUR SCRIPT
 
 COMMANDS=()
 COMMAND_DESC=()
@@ -26,15 +26,15 @@ usage(){
 
 ## 2: MINIMAKE DEFINE YOUR COMMANDS
 
+## YOUR CODE HERE
+
 ## REMOVE THIS WHEN YOU GET THE PICTURE
 make_command "demo" "this command is for explaining how minimake works"
 demo(){
   echo "This is a demo command"
 }
 
-## YOUR CODE HERE
-
-## 3:MINIMAKE EXECUTION
+## 3:MINIMAKE EXECUTION. PLACE THIS AT THE BOTTOM OF YOUR SCRIPT
 if test "$#" -eq 1 &&  [[ " ${COMMANDS[@]} " =~ $1 ]]; then
   eval "$1"
 else
